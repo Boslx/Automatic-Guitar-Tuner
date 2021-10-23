9ca185e880233d3fe297f15d18b3687c2446211e
LeoDJ <leandro.spaeth@gmail.com>
2021-10-23 03:20:15 +0200

    implement serial servo control

--- GuitarAutoTune_Firmware/src/main.cpp
+++ GuitarAutoTune_Firmware/src/main.cpp
@@ -5,30 +5,55 @@
 Servo tuningServos[NUM_TUNING_SERVOS];
 const uint8_t tuningServoPins[] = {7, 8, 9, 10, 11, 12};
 
+
+uint8_t curServoIdx = 0;
+String curString;
+
+void parseReceivedMessage() {
+    if (curServoIdx < NUM_TUNING_SERVOS) {
+        int32_t val = curString.toInt();
+        if (val >= 700 && val <= 2300) {
+            tuningServos[curServoIdx].writeMicroseconds(val);
+            // Serial.println("Setting servo " + String(curServoIdx) + " to " + String(val));
+        }
+        else {
+            // Serial.println("Received wrong value " + String(curString));
+        }
+    }
+}
+
+void handleSerialByte(char c) {
+    switch (c) {
+        case ';':
+        case '\n':
+            parseReceivedMessage();
+            curString = "";
+            curServoIdx = 0;
+            break;
+        case ',':
+            parseReceivedMessage();
+            curString = "";
+            curServoIdx++;
+            break;
+        default:
+            curString += c;
+            break;
+    }
+    Serial.print(c);
+}
+
 void setup() {
     pinMode(13, OUTPUT);
     Serial.begin(115200);
 
     for (int i = 0; i < NUM_TUNING_SERVOS; i++) {
         tuningServos[i].attach(tuningServoPins[i]);
+        tuningServos[i].writeMicroseconds(1500);
     }
 }
 
 void loop() {
-    for (int val = 1000; val < 2000; val += 10) {
-        for (int i = 0; i < NUM_TUNING_SERVOS; i++) {
-            tuningServos[i].writeMicroseconds(val);
-        }
-        delay(10);
-        Serial.println(val);
+    while (Serial.available()) {
+        handleSerialByte(Serial.read());
     }
-    delay(500);
-    for (int val = 2000; val > 1000; val -= 10) {
-        for (int i = 0; i < NUM_TUNING_SERVOS; i++) {
-            tuningServos[i].writeMicroseconds(val);
-        }
-        delay(10);
-        Serial.println(val);
-    }
-    delay(500);
 }
